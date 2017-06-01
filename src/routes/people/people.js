97f50b409160a3f06c0a84c7a96b48e7d855921b
Erik Grinaker <erik@bengler.no>
2017-06-01 10:22:00 +0100

    big P in people

---
+++ src/routes/people/people.js
@@ -0,0 +1,53 @@
+/**
+ * React Starter Kit (https://www.reactstarterkit.com/)
+ *
+ * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import s from './People.css';
+import Link from '../../components/Link';
+
+
+class Persons extends React.Component {
+
+  static propTypes = {
+    persons: PropTypes.arrayOf(
+      PropTypes.shape({
+        name: PropTypes.string,
+        references: PropTypes.number,
+      }),
+    ).isRequired,
+  }
+
+  render() {
+    const { persons } = this.props;
+    return (
+      <div>
+        <div className={s.container}>
+          <h1 className={s.title}>Persons</h1>
+          <ul>
+            {
+              persons.map((person) => {
+                const id = person._id;
+                return (
+                  <li key={person._id}>
+                    <Link to={`/person/${id}`}>{person.name || 'No name…'}</Link>
+                    <span style={{ fontWeight: '400' }}> {person.references}</span>
+                  </li>
+                );
+              })
+            }
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withStyles(s)(Persons);
