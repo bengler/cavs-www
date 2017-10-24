fbaa5aad449bfe7472b6e77ab29cc3ca4c0058f9
Kristoffer Sivertsen <kristofferjoachim@gmail.com>
2017-10-24 16:54:09 +0200

    Adding footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,28 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import withStyles from 'isomorphic-style-loader/lib/withStyles'
 import s from './Footer.css'
 
 class Footer extends React.PureComponent {
+  static propTypes = {
+    inverted: PropTypes.bool
+  }
+
+  static defaultProps = {
+    inverted: false
+  }
+
   render() {
     return (
       <div className={s.root}>
-        <div className={s.container}>
-          <span className={s.text}>© MIT Center for Advanced Visual Studies Archive</span>
+        <img className={s.actLogo} src="/images/act_black_rgb_RZ.svg" />
+        <div className={s.mit}>
+          School of Architecture and Planning
+          Massachusetts Institute of Technology
+          Cambridge, Massachusetts
+        </div>
+        <div className={s.contact}>
+          Contact, About the site, Giving, Facebook, Twitter
         </div>
       </div>
     )
